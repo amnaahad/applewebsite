e8400a9f369efe4e2ace0d747eb3744c4a7d0aee
amnaahad <amnakhan200427@gmail.com>
2024-09-05 23:17:02 +0500

    changes

---
+++ 3d/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+const App=()=> {
+
+
+  return (
+    <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+    </main>
+  )
+}
+
+export default App
---
+++ 3d/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className='h-5/6 w-full flex-center flex-col'>
+      <p className='hero-title'>iPhone 15 Pro</p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
